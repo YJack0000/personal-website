18c35e84f66d22b89a98a3b2481418c1d43b85aa
YJack0000 <jackleagends@gmail.com>
2022-10-12 02:57:57 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import Motto from "../components/Motto.vue";
+</script>
+<template>
+  <div class="mr-auto ml-auto pt-14 max-w-md">
+    <h1 class="text-slate-50">This is an about page</h1>
+
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <Motto />
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+    <h1 class="text-slate-50">This is an about page</h1>
+  </div>
+</template>
